35e1d7f5655eb16cf8ec2d3f1dd8458c0b1aab12
lruler <1204312199@qq.com>
2021-12-04 14:36:45 +0800

    add icon

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import './importIcons';
+import classes from '../../static/classes';
 
-interface IconProps {
-  name?: string;
+interface IconProps extends React.SVGAttributes<SVGElement> {
+  name: string;
 }
 
-const Icon: React.FC<IconProps> = ({ name = '' }) => {
-  return <span>Icon</span>;
+const Icon: React.FunctionComponent<IconProps> = ({
+  className,
+  name,
+  ...restProps
+}) => {
+  return (
+    <svg className={classes('fui-icon', className)} {...restProps}>
+      <use xlinkHref={`#${name}`} />
+    </svg>
+  );
 };
 
 export default Icon;
